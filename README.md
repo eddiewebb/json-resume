4c3a8a1f16a98f23c26ff007fbfca0b583b3e48a
EDWARD WEBB <EDWARD.WEBB@LibertyMutual.com>
2018-01-31 16:20:37 -0500

    adding all the required static files to publish

---
+++ README.md
@@ -0,0 +1,32 @@
+# Data Fed resume
+
+Created from [Start Bootstrap - Resume](https://startbootstrap.com/template-overviews/resume/), augmented with Handlebars for dynamic data entry.
+
+Published via Bitbucket pipelines to [Eddie's Website](https://edwardawebb.com)
+
+
+
+
+
+
+
+
+
+## About (from Start Bootstrap)
+
+Start Bootstrap is an open source library of free Bootstrap templates and themes. All of the free templates and themes on Start Bootstrap are released under the MIT license, which means you can use them for any purpose, even for commercial projects.
+
+* https://startbootstrap.com
+* https://twitter.com/SBootstrap
+
+Start Bootstrap was created by and is maintained by **[David Miller](http://davidmiller.io/)**, Owner of [Blackrock Digital](http://blackrockdigital.io/).
+
+* http://davidmiller.io
+* https://twitter.com/davidmillerskt
+* https://github.com/davidtmiller
+
+Start Bootstrap is based on the [Bootstrap](http://getbootstrap.com/) framework created by [Mark Otto](https://twitter.com/mdo) and [Jacob Thorton](https://twitter.com/fat).
+
+## Copyright and License
+
+Copyright 2013-2018 Blackrock Digital LLC. Code released under the [MIT](https://github.com/BlackrockDigital/startbootstrap-resume/blob/gh-pages/LICENSE) license.
---
+++ data/experience.js
@@ -0,0 +1,44 @@
+var experienceInventory = [
+    {
+        "role":"Director, Enterprise Software Delivery",
+        "company":"Liberty Mutual Technology Group",
+        "summary":"Led in development and oftware delivery and enterprise cloud adoption strategy. Collaborating across IT functions (security, compliance, networking, etc) to expand automation and self-service to reduce time for new applications from weeks to days.",
+        "range":"February 2016 - Present"
+    },
+    {
+        "role":"Manager, Enterprise Software Developer",
+        "company":"Liberty Mutual Technology Group",
+        "summary":"Led team delivering Liberty Mutual's first PaaS, modernizing software delivery. Accelerated tge adoption of cloud delivery and deployment platforms by 800%.",
+        "range":"March 2014 - February 2016"
+    },
+    {
+        "role":"Principal Software Developer",
+        "company":"Liberty Mutual Technology Group",
+        "summary":"",
+        "range":""
+    },
+    {
+        "role":"Senior Software Developer",
+        "company":"Liberty Mutual Technology Group",
+        "summary":"",
+        "range":""
+    },
+    {
+        "role":"Software Developer",
+        "company":"Liberty Mutual Technology Group",
+        "summary":"",
+        "range":""
+    },
+    {
+        "role":"Associate (Jr) Software Developer",
+        "company":"Liberty Mutual Technology Group",
+        "summary":"",
+        "range":""
+    },
+    {
+        "role":"IT Analyst",
+        "company":"Liberty Mutual Technology Group",
+        "summary":"",
+        "range":""
+    }
+]
