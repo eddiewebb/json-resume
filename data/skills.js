40d7dce4cbadd7f51dc4760a3c4b684c256511e5
Eddie Webb <ollitech@gmail.com>
2018-02-11 13:50:28 -0500

    Revert "Converts site to static generation via HUGO"

---
+++ data/skills.js
@@ -0,0 +1,19 @@
+var skillsInventory = [
+        {
+            "name":"Languages, Operating Systems & Tools",
+            "entries":[ "Java","Python","git","subversion","linux","bash","php","jquery","javascript"]
+        },
+        {
+            "name":"Platform Development & Administration",
+            "entries":["Atlassian","Bamboo","Bitbucket","NGINX","MySQL","Wordpress"]
+        },
+            {"name":"Containers & Cloud",
+            "entries":[
+                {name:"BOSH",link:"https://bosh.io"},
+                {name:"Docker",link:"https://docker.com"},
+                {name:"AWS",link:"https://aws.amazon.com"},
+                {name:"Google Cloud",link:"https://cloud.google.com"},
+                {name:"Linux",link:"https://bosh.io"}
+            ]
+        }
+    ]
